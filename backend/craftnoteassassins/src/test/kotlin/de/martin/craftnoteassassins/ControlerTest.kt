349db9c5b1b0f12c0c3e71f70d340f8d980a6f4d
Martin <martin.schwarzmaier@gmail.com>
2019-08-27 01:11:12 +0200

    remote unused import

--- backend/craftnoteassassins/src/test/kotlin/de/martin/craftnoteassassins/ControlerTest.kt
+++ backend/craftnoteassassins/src/test/kotlin/de/martin/craftnoteassassins/ControlerTest.kt
@@ -2,24 +2,16 @@ package de.martin.craftnoteassassins
 
 import com.google.gson.Gson
 import de.martin.craftnoteassassins.dtos.UserDTO
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
-import org.springframework.security.test.context.support.WithMockUser
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import sun.plugin2.util.PojoUtil.toJson
-import sun.plugin2.util.PojoUtil.toJson
-
-
-
 
 
 @ExtendWith(SpringExtension::class)
